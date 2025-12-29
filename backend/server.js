0f8c147b8948c8adc1b7240ca221324435f63bf8
Riya Mohile <riyamohile812@gmail.com>
2025-12-29 12:52:38 +0530

    add frontend live url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-name.vercel.app",
+      "https://task-frontend-silk-tau.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
